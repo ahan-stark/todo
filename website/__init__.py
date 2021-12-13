e6116c6f07bbb7abe2220b8c8a9cda5fa9338368
ahan-strak <ahanabubakar@gmail.com>
2021-12-13 20:01:01 +0530

    first commit

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
-from flask_login import LoginManager, login_manager
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -9,7 +9,7 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'djwjwkrwkrl'
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
@@ -21,9 +21,7 @@ def create_app():
 
     from .models import User, Note
 
-    
     create_database(app)
-
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
--- website/static/index.js
+++ website/static/index.js
@@ -1,8 +1,8 @@
-function deleteNote(noteID) {
-  fetch("/delete", {
-      method:"POST",
-      body:JSON.stringify({ noteID: noteID }),
-      }).then((_res)=> {
-          window.location.href="/";
-      });
-}
+function deleteNote(noteId) {
+    fetch("/delete-note", {
+      method: "POST",
+      body: JSON.stringify({ noteId: noteId }),
+    }).then((_res) => {
+      window.location.href = "/";
+    });
+  }
